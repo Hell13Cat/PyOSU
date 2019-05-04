7b91d707a3a8b919b0cc6d4aa33a0e81aec9956b
Hell13Cat <blacktigermaxx@gmail.com>
2019-05-05 00:01:16 +0300

    Update __init__.py
    Add 2 custom error.

--- pyosu/__init__.py
+++ pyosu/__init__.py
@@ -1,11 +1,19 @@
 import requests, json
 
-class start():
+class Errorgen(Exception):
+    def __init__(self, text):
+        self.txt = text
+
+
+class main():
     def __init__(self, token):
         self.base_url = "https://osu.ppy.sh/api/"
         self.token = token
     def jsong(self, url):
-        data=requests.get(url)
+        try:
+            data=requests.get(url)
+        except:
+            raise Errorgen("No Connect to server")
         return data.json()
     def get_user(self, name, mode, typedata=None):
         ready_url = self.base_url + "get_user?k=" + self.token + "&u=" + name + "&m=" + str(mode)
@@ -13,14 +21,20 @@ class start():
         if typedata==None:
             return data_get
         else:
-            return data_get[typedata]
+            try:
+                return data_get[typedata]
+            except:
+                raise Errorgen("Type data error: " + typedata)
     def get_user_best(self, name, mode, typedata=None):
         ready_url = self.base_url + "get_user_best?k=" + self.token + "&u=" + name + "&m=" + str(mode) + "&limit=1"
         data_get = self.jsong(ready_url)[0]
         if typedata==None:
             return data_get
         else:
-            return data_get[typedata]
+            try:
+                return data_get[typedata]
+            except:
+                raise Errorgen("Type data error: " + typedata)
     def get_user_recent(self, name, mode, typedata=None):
         ready_url = self.base_url + "get_user_recent?k=" + self.token + "&u=" + name + "&m=" + str(mode) + "&limit=1"
         try:
@@ -30,4 +44,7 @@ class start():
         if typedata==None:
             return data_get
         else:
-            return data_get[typedata]
+            try:
+                return data_get[typedata]
+            except:
+                raise Errorgen("Type data error: " + typedata)
