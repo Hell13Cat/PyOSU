9333d49d697c7d7cb2b51b88698cdb44ea907f67
Hell13Cat <blacktigermaxx@gmail.com>
2019-05-05 00:06:20 +0300

    Update __init__.py
    Remove error

--- pyosu/__init__.py
+++ pyosu/__init__.py
@@ -1,19 +1,11 @@
 import requests, json
 
-class Errorgen(Exception):
-    def __init__(self, text):
-        self.txt = text
-
-
 class main():
     def __init__(self, token):
         self.base_url = "https://osu.ppy.sh/api/"
         self.token = token
     def jsong(self, url):
-        try:
-            data=requests.get(url)
-        except:
-            raise Errorgen("No Connect to server")
+        data=requests.get(url)
         return data.json()
     def get_user(self, name, mode, typedata=None):
         ready_url = self.base_url + "get_user?k=" + self.token + "&u=" + name + "&m=" + str(mode)
@@ -21,20 +13,14 @@ class main():
         if typedata==None:
             return data_get
         else:
-            try:
-                return data_get[typedata]
-            except:
-                raise Errorgen("Type data error: " + typedata)
+            return data_get[typedata]
     def get_user_best(self, name, mode, typedata=None):
         ready_url = self.base_url + "get_user_best?k=" + self.token + "&u=" + name + "&m=" + str(mode) + "&limit=1"
         data_get = self.jsong(ready_url)[0]
         if typedata==None:
             return data_get
         else:
-            try:
-                return data_get[typedata]
-            except:
-                raise Errorgen("Type data error: " + typedata)
+            return data_get[typedata]
     def get_user_recent(self, name, mode, typedata=None):
         ready_url = self.base_url + "get_user_recent?k=" + self.token + "&u=" + name + "&m=" + str(mode) + "&limit=1"
         try:
@@ -44,7 +30,4 @@ class main():
         if typedata==None:
             return data_get
         else:
-            try:
-                return data_get[typedata]
-            except:
-                raise Errorgen("Type data error: " + typedata)
+            return data_get[typedata]
