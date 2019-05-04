9ad753acc66e3ee16fe97b764e2882ddccfec822
Hell13Cat <blacktigermaxx@gmail.com>
2019-05-05 00:23:47 +0300

    0.1b1
    Add:
    scores
    Rename:
    get_user ->user
    get_user_best ->user_best
    get_user_recent -> user_recent

--- pyosu/__init__.py
+++ pyosu/__init__.py
@@ -7,21 +7,28 @@ class main():
     def jsong(self, url):
         data=requests.get(url)
         return data.json()
-    def get_user(self, name, mode, typedata=None):
+    def user(self, name, mode, typedata=None):
         ready_url = self.base_url + "get_user?k=" + self.token + "&u=" + name + "&m=" + str(mode)
         data_get = self.jsong(ready_url)[0]
         if typedata==None:
             return data_get
         else:
             return data_get[typedata]
-    def get_user_best(self, name, mode, typedata=None):
+    def user_best(self, name, mode, typedata=None):
         ready_url = self.base_url + "get_user_best?k=" + self.token + "&u=" + name + "&m=" + str(mode) + "&limit=1"
         data_get = self.jsong(ready_url)[0]
         if typedata==None:
             return data_get
         else:
             return data_get[typedata]
-    def get_user_recent(self, name, mode, typedata=None):
+    def scores(self, name, mapid, mode, typedata=None):
+        ready_url = self.base_url + "get_scores?k=" + self.token + "&u=" + name + "&b=" + mapid + "&m=" + str(mode) + "&limit=1"
+        data_get = self.jsong(ready_url)[0]
+        if typedata==None:
+            return data_get
+        else:
+            return data_get[typedata]
+    def user_recent(self, name, mode, typedata=None):
         ready_url = self.base_url + "get_user_recent?k=" + self.token + "&u=" + name + "&m=" + str(mode) + "&limit=1"
         try:
             data_get = self.jsong(ready_url)[0]
