d3a8078e971c8d3528f05088ada10ad442e9e958
Hell13Cat <blacktigermaxx@gmail.com>
2019-05-05 03:12:36 +0300

    1.1

--- setup.py
+++ setup.py
@@ -6,13 +6,13 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pyosu",
-    version="1.0",
+    version="1.1",
     author="HellCatNya",
     author_email="blacktigermaxx@gmail.com",
     description="Python lib for OSU API",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/Hell13Cat/PyOSU",
+    url="https://github.com/Hell13Cat/PyOSU/",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
